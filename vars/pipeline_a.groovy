47c4620b1f5324fbd977daed6ff9b4ff53c3ec72
ron sh <ronshilo@gmail.com>
2020-10-28 07:43:25 +0200

    Update pipeline_a.groovy

--- vars/pipeline_a.groovy
+++ vars/pipeline_a.groovy
@@ -1,5 +1,4 @@
-def call(int buildNumber) {
-  if (buildNumber % 2 == 0) {
+def call() {
     pipeline {
       agent any
       stages {
@@ -10,16 +9,4 @@ def call(int buildNumber) {
         }
       }
     }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-      }
-    }
-  }
 }
