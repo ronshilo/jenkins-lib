6ea9171d43cad42ea7d1ce43157667aa7e73d8b6
ron sh <ronshilo@gmail.com>
2020-10-28 07:48:06 +0200

    Update pipeline_a.groovy

--- vars/pipeline_a.groovy
+++ vars/pipeline_a.groovy
@@ -1,12 +1,7 @@
 def call() {
-    pipeline {
-      agent any
-      stages {
         stage('Even Stage') {
           steps {
             echo "The build number is even"
           }
         }
-      }
-    }
 }
